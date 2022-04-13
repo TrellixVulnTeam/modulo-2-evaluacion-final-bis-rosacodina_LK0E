f8cb6258e8da91294cd4282e358a8c19539f315b
Rosa Codina <rosa.codina90@gmail.com>
2022-04-13 22:29:20 +0200

    fetch done

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,29 @@
 'use strict';
 
 console.log('>> Ready :)');
+//CONSTANTS
 
+//cuando la pag cargue que se haga una peticion a la api con fecth, relleno un arrya con lo que devuelva la api. funcion que caundo la web cargue que se imprima consola "he cargado" y caundo eso pase meto el fecth y luego lo meto en la lista y después inteno implementar el onloa
 //VARIABLES
 let users = [];
+
+//FUNCTIONS
 
 //I used fetch to take the data from an external site
 function fetchApi() {
   fetch('https://randomuser.me/api/?results=10')
     .then((response) => response.json())
     .then((data) => {
-      users = data;
+      users = data.results;
       console.log(users);
     });
+}
+
+console.log('enlazado');
+
+function myFunction() {
+  alert('Page in load');
+  console.log('carga');
 }
 
 //# sourceMappingURL=main.js.map
